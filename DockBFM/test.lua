f7e001e3e61846e743f2278b8d6971991d29c722
Mark Nelson <markerdink@gmail.com>
2013-08-28 07:29:21 -0700

    lolz

--- DockBFM/test.lua
+++ DockBFM/test.lua
@@ -1,4 +1,4 @@
 local monitor = peripheral.wrap("top");
 term.redirect(monitor);
-term.write("Boat House Monitor - test");
-term.write("I'm on a muther fucking boat!");
+term.write("Boat House Monitor - test\r\n");
+term.write("I'm on a muther fucking boat!\r\n");
