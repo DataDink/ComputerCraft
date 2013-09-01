260b194fdd9efe33a2550966f6f647a029ae4cd5
Mark Nelson <markerdink@gmail.com>
2013-09-01 14:37:49 -0700

    excavator

---
+++ Libraries/excavator.lua
@@ -0,0 +1,206 @@
+if (excavator == nil) then 
+	excavator = {};
+	
+	(function() 
+		local directions = {
+			forward = 270, front = 270,
+			rear = 90, back = 90, backward = 90,
+			left = 180, 
+			right = 0,
+			up = "up",
+			down = "down"
+		};
+	
+		local position = {
+			marker = { x = 0, y = 0, z = 0 },
+			target = { x = 0, y = 0, z = 0 },
+			current = { x = 0, y = 0, z = 0, d = directions.forward },
+			mark = function() position.marker.x = position.current.x; position.marker.y = position.current.x; position.marker.z = position.current.z; end,
+			reset = function() position.current.x = 0; position.current.y = 0; position.current.z = 0; position.current.d = directions.forward; end
+		};
+		
+		local fuel = {};
+		local move = {};
+		local inventory = {};
+		
+		-- misc
+		local alternate = function(from, to, step)
+			if (step == nil) then
+				step = 1;
+				if (from > to) then step = -1; end
+			end
+			local index = from - step;
+			return function()
+				index = index + step;
+				local isForward = to > from;
+				local isBackward = to < from;
+				local reachedEnd = isForward and index >= to or isBackward and index <= to;
+				local reachedStart = isForward and index < from or isBackward and index > from;
+				if (reachedEnd) then
+					index = to;
+					step = -step;
+				elseif (reachedStart) then
+					return;
+				end
+				return index;
+			end
+		end
+		
+		-- Fuel
+		fuel.movesPerFuel = 0;
+		fuel.calcRemainingFuel = function() return turtle.getItemCount(1) * movesPerFuel + turtle.getFuelLevel(); end
+		fuel.needsRefuel = function() return move.calcReturnDist() >= fuel.calcRemainingFuel(); end
+		fuel.initialize = function()
+			local before = turtle.getFuelLevel();
+			turtle.select(inventory.fuelSlot);
+			turtle.refuel(1);
+			local after = turtle.getFuelLevel();
+			fuel.movesPerFuel = after - before;
+		end
+		fuel.refuel = function()
+			if (turtle.getFuelLevel() > 0) then return; end
+			fuel.initialize();
+		end
+		
+		-- Inventory
+		inventory.fuelSlot = 1;
+		inventory.calcRemainingSlots = function() 
+			local count = 0;
+			for i = 2, 16 do
+				if (turtle.getItemCount(i) == 0) then count = count + 1; end
+			end
+			return count;
+		end
+		inventory.needsUnload = function() return inventory.calcRemainingSlots() == 0; end
+		inventory.unload = function()
+			move.face(directions.backward);
+			for i = 2, 16 do
+				if (turtle.getItemCount(i) > 0) then
+					turtle.select(i);
+					if (not turtle.drop()) then
+						error("Aborting: Can't unload inventory.");
+					end
+				end				
+			end
+		end
+		
+		-- Movement
+		move.calcReturnDist = function() return math.abs(target.x - position.x) + math.abs(target.y - position.y) + math.abs(target.z - position.z); end
+		move.face = function(direction)
+			if (direction == directions.up or direction == directions.down or direction == position.current.d) then return; end
+			if (direction == (position.current.d + 270) % 360) then 
+				turtle.turnLeft();
+				position.current.d = direction;
+			else
+				while (direction > position.current.d) do
+					turtle.turnRight();
+					position.current.d = (position.current.d + 90) % 360;
+				end
+			end
+		end
+		move.direction = function(direction)
+			sleep(0.01);
+			move.face(direction);
+			turtle.digUp(); turtle.digDown(); turtle.dig();
+			fuel.refuel();
+			
+			if (direction == directions.up and turtle.detectUp()) then return false; end
+			if (direction == directions.down and turtle.detectDown()) then return false; end
+			if (turtle.detect()) then return false; end
+			
+			local method = turtle.forward;
+			if (direction == directions.up) then method = turtle.up end
+			if (direction == directions.down) then  method = turtle.down end
+			if (method()) then
+				if (direction == directions.up) then position.current.z = position.current.z + 1; 
+				elseif (direction == directions.down) then position.current.z = position.current.z - 1; 
+				elseif (direction == directions.front) then position.current.y = position.current.y + 1; 
+				elseif (direction == directions.back) then position.current.y = position.current.y - 1; 
+				elseif (direction == directions.left) then position.current.x = position.current.x - 1; 
+				elseif (direction == directions.right) then position.current.x = position.current.x + 1; end
+				return true;
+			else
+				return false;
+			end
+		end
+		move.to = function(x, y, z)
+			while (position.current.z < z) do if (not move.direction(directions.up)) then return false; end end
+			while (position.current.z > z) do if (not move.direction(directions.down)) then return false; end end
+			while (position.current.x < x) do if (not move.direction(directions.right)) then return false; end end
+			while (position.current.x > x) do if (not move.direction(directions.left)) then return false; end end
+			while (position.current.y < y) do if (not move.direction(directions.forward)) then return false; end end
+			while (position.current.y > y) do if (not move.direction(directions.backward)) then return false; end end
+			return true;
+		end
+		move.home = function(callback)
+			position.mark();
+			move.to(0, 0, 0);
+			if (callback ~= nil) then
+				callback();
+				move.to(position.marker.x, position.marker.y, position.marker.z);
+			end
+		end
+		move.finish = function()
+			move.home();
+			inventory.unload();
+			turtle.select(1);
+			turtle.drop();
+		end
+		move.excavate = function(x, y, z)
+			position.reset();
+			fuel.initialize();
+			if (fuel.calcRemainingFuel() < 4) then
+				error("Aborting: Insufficient fuel.");
+			end
+			
+			local startz = math.min(2, math.abs(z));
+			if (z < 0) then startz = -startz; end
+			local zstep = startz / 2 * 3;
+			
+			for layer = startz, z, zstep do
+				if (not move.to(position.current.x, position.current.y, layer)) then
+					print("Returning: Reached unbreakable blocks");
+					move.finish();
+					return;
+				end
+				
+				for column in alternate(0, y) do
+					for row in alternate(0, x) do
+						print("DEBUG: Moving to " .. row .. ", " .. column .. ", " .. layer);
+					
+						if (fuel.needsRefuel()) then
+							print("Returning for refuel...");
+							move.home(function() 
+								print("Waiting for more fuel in slot 1.");
+								turtle.refuel();
+								while (not turtle.refuel(1)) do
+									sleep(3);
+									turtle.select(1);
+								end
+								turtle.initialize();
+								print("Continuing");
+							end);
+						end
+						
+						if (inventory.needsUnload()) then
+							print("Returning for unload...");
+							move.home(function() 
+								inventory.unload();
+								print("Continuing");
+							end);
+						end
+						
+						if (not move.to(row, column, layer)) then
+							print("Returning: Reached unbreakable blocks");
+							move.finish();
+						end
+					end
+				end
+			end
+		end
+	
+		excavator.start = function(x, y, z)
+			move.excavate(x, y, z);
+		end
+	end)();
+end
